1138f51f507d35597f69349295def4408ca6859a
Andrea Civita <andreacivita994@gmail.com>
2020-11-26 01:32:21 +0100

    Test css webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,22 @@ const mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
+ "public/vendor/css/bootstrap.js"
+ "public/vendor/css/jquery.js"
+ "public/vendor/css/jquery.fittext.js"
+ "public/vendor/css/jquery-ui.js"
+ "public/vendor/css/moment.js"
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+  'public/vendor/css/bootstrap.css',
+  'public/vendor/css/jquery-ui.css',
+  'public/vendor/css/vegas.min.css',
+  'public/static/css/calendario.css',
+  'public/static/css/interne.css',
+  'public/static/css/bootstrap-italia_1.2.0.min.css',
+  'public/static/css/titilliumwebfont.css',
+  'public/static/css/cagliari.min.css',
+  'public/static/css/cagliari-print.min.css',
+],'public/build/gulliver.min.css');
